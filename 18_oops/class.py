8891129e2d40f8ed42d6ce5c5c8b078c3a15aa50
Abhi <amodi2106@gmail.com>
2026-07-25 18:43:17 +0530

    changes

--- 18_oops/class.py
+++ 18_oops/class.py
@@ -5,6 +5,6 @@ class Student:
         print(self)             #it will print address of current object
         return 20
     
-s2 = Student()          #object creation
+s2 = Student()          
 print(s2.get_age())       #accessing method using object
 print(s2.Name)          #accessing attribute using object
