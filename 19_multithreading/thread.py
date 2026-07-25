240c8c0edfcda477e8e0011e2446b5c3cc417651
Abhi <amodi2106@gmail.com>
2026-07-25 19:28:49 +0530

    threading

--- 19_multithreading/thread.py
+++ 19_multithreading/thread.py
@@ -1,1 +1,22 @@
 #run program or functions parallelly
+import time
+import random
+from concurrent.futures import ThreadPoolExecutor
+
+tables = ["products" , "customers" , "orders" , "cancel" , "reviews"]
+
+def myfun(i):
+
+        wait = random.randint(1,10)
+        time.sleep(wait )
+        print(f"i'm {i}.i'm took {wait} sec")
+
+# for i in tables:
+#     myfun(i)
+
+#loops are slow so insted of loop we use thread
+
+with ThreadPoolExecutor(max_workers=len(tables)) as executor:
+    futures = executor.map(myfun,tables)
+        
+
